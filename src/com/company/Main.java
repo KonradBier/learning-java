7185229ef0319fb74aba759a919715b7e92d18af
Konrad Biernacki <konradbiernacki@gmail.com>
2022-01-26 16:42:36 +0100

    Next commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String raining1 = "AA";
+	    String raining2 = "BB";
+	    String raining3 = "CC";
+
+        System.out.println(raining1);
+        System.out.println(raining2);
+        System.out.println(raining3);
+        System.out.println("-----");
+
+        raining2 = raining1;
+        System.out.println(raining1);
+        System.out.println(raining2);
+        System.out.println("-----");
+
+        raining1 = "DD";
+        System.out.println(raining1);
+        System.out.println(raining2);
+        System.out.println("-----");
+
     }
 }
