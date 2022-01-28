9d45a4fe163e7f540cd77764b0a8d2385fa4f552
Konrad Biernacki <konradbiernacki@gmail.com>
2022-01-28 17:54:16 +0100

    Methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,15 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-	    String raining1 = "AA";
-	    String raining2 = "BB";
-	    String raining3 = "CC";
-
-        System.out.println(raining1);
-        System.out.println(raining2);
-        System.out.println(raining3);
-        System.out.println("-----");
-
-        raining2 = raining1;
-        System.out.println(raining1);
-        System.out.println(raining2);
-        System.out.println("-----");
-
-        raining1 = "DD";
-        System.out.println(raining1);
-        System.out.println(raining2);
-        System.out.println("-----");
-
+        System.out.println(
+                "Witajcie! Kwadrat 10 to: " + policzKwadrat(10)
+        );
+        System.out.println("Policzone!");
+    }
+    public static int policzKwadrat(int liczba) {
+        System.out.println("Liczymy kwadrat liczby " + liczba);
+        return liczba * liczba;
     }
 }
+
