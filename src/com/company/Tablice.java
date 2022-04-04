617445370ed555717201c26005359c681e779d56
Konrad Biernacki <konradbiernacki@gmail.com>
2022-04-04 14:11:16 +0200

    Next exercise + improvements

--- src/com/company/Tablice.java
+++ src/com/company/Tablice.java
@@ -13,24 +13,11 @@ public class Tablice {
     static int sumArray(int[][] numbers) {
         int sum = 0;
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i].length > 1) {
                 for (int j = 0; j < numbers[i].length; j++) {
-                    sum = sum + numbers[i][j];
+                    sum += numbers[i][j];
                     System.out.println(i + " " + j);
                 }
-            } else {
-                System.out.println(i);
-                sum = sum + numbers[i][0];
             }
-        }
         return sum;
     }
 }
-
-
-/**
- *             for (int j = 0; j < numbers[i].length -1; j++) {
- *                 sum = sum + numbers[i][j];
- *                 System.out.println(i + " " + j);
- *                 System.out.println("-------------");
- */
